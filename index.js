52a47a3dec16b86c1ad660d08a12931d33212ca4
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-07-08 12:44:27 -0400

    pushed all files

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("Server is up");
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("call ended bro");
+  });
+
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+  });
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+server.listen(PORT, () => console.log(`server listening on port ${PORT}`));
